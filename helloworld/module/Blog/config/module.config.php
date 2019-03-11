6954eea1a5fc4432d9b470c8c9901425f5f94dc4
12aleksandr12 <12aleksandr12@i.ua>
2019-03-11 12:34:08 +0200

    add blog new

--- helloworld/module/Blog/config/module.config.php
+++ helloworld/module/Blog/config/module.config.php
@@ -6,41 +6,36 @@ use Zend\ServiceManager\Factory\InvokableFactory;
 use Zend\Router\Http\Segment;
 use Blog\Controller\ListController;
 
-return [
-    'service_manager' => array(
-        'invokables' => array(
-            'Blog\Service\PostServiceInterface' => 'Blog\Service\PostService'
-        )
-    ),
-    'view_manager' => [
-        'template_path_stack' => [
+return array(
+    'view_manager' => array(
+        'template_path_stack' => array(
             __DIR__ . '/../view',
-        ],
-    ],
-    'controllers'  => array(
-        'factories' => array(
-            'Blog\Controller\List' => 'Blog\Factory\ListControllerFactory'
+        ),
+    ),
+    'controllers' => array(
+        'invokables' => array(
+            'Blog\Controller\List' => 'Blog\Controller\ListController'
         )
     ),
     // This lines opens the configuration for the RouteManager
-    'router' => [
+    'router' => array(
         // Open configuration for all possible routes
-        'routes' => [
+        'routes' => array(
             // Define a new route called "post"
-            'post' => [
+            'post' => array(
                 // Define the routes type to be "Zend\Mvc\Router\Http\Literal", which is basically just a string
                 'type' => 'literal',
                 // Configure the route itself
-                'options' => [
+                'options' => array(
                     // Listen to "/blog" as uri
                     'route'    => '/blog',
                     // Define default controller and action to be called when this route is matched
-                    'defaults' => [
+                    'defaults' => array(
                         'controller' => 'Blog\Controller\List',
                         'action'     => 'index',
-                    ]
-                ]
-            ]
-        ]
-    ]
-];
+                    )
+                )
+            )
+        )
+    )
+);
--- helloworld/module/Blog/src/Blog/Controller/ListController.php
+++ helloworld/module/Blog/src/Blog/Controller/ListController.php
@@ -2,18 +2,8 @@
 // Filename: /module/Blog/src/Blog/Controller/ListController.php
 namespace Blog\Controller;
 
-use Blog\Service\PostServiceInterface;
 use Zend\Mvc\Controller\AbstractActionController;
 
 class ListController extends AbstractActionController
 {
-    /**
-     * @var \Blog\Service\PostServiceInterface
-     */
-    protected $postService;
-
-    public function __construct(PostServiceInterface $postService)
-    {
-        $this->postService = $postService;
-    }
 }
